dc02a51f21866b90c26b9bae43069d5e8d277ed3
Magesh <tkmagesh77@gmail.com>
2018-03-26 17:31:25 +0530

    Day-01 Complete

--- first-app/src/app/greeter/greeter.component.ts
+++ first-app/src/app/greeter/greeter.component.ts
@@ -9,8 +9,7 @@ export class GreeterComponent{
 	//userName : string = '';
 	greetMessage : string = '[This is the default message]';
 
-	greet(){
-		let userName = prompt('Enter the username :');
+	greet(userName : string){
 		this.greetMessage = `Hi ${userName}, Have a nice day!`;
 	}
 }
