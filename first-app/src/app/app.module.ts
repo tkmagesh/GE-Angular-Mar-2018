c7cba58f0daa4c3fa120b4fbf6ebca5de61855d6
Magesh <tkmagesh77@gmail.com>
2018-03-27 11:15:36 +0530

    calculator two solution

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 
 import { GreeterComponent } from './greeter/greeter.component';
 import { CalculatorOneComponent } from './calculator/calculatorOne.component';
+import { CalculatorTwoComponent } from './calculator/calculatorTwo.component';
 
 @NgModule({
   declarations: [
     AppComponent
     , GreeterComponent
     , CalculatorOneComponent
+    , CalculatorTwoComponent
   ],
   imports: [
     BrowserModule
